026536243b3ac60b2921746b16d13c44a4026507
trevorColby <trevor.john.colby@gmail.com>
2017-12-27 14:48:44 -0500

    Added quotes to url in main.js for background image

--- pages/homepage/typescript/main.js
+++ pages/homepage/typescript/main.js
@@ -3,7 +3,7 @@
 //Main Typescript for homepage of dartmouth.life
 
 function paintBackground(){
-	document.body.style.backgroundImage = url('https://trevorColby.github.io/media/homepage/pictures/HopPhoto.jpg');
+	document.body.style.backgroundImage = "url('https://trevorColby.github.io/media/homepage/pictures/HopPhoto.jpg')";
 	alert("Function Called");
 	console.log("background loaded");
 }
