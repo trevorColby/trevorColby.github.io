9dfa57b4af8d716e738e7cb9955d147a6969011c
trevorColby <trevor.john.colby@gmail.com>
2017-12-27 12:02:32 -0500

    Updated index.html to reference main.js and created main.js
    This replaces main.ts, attempting to fix reference/ get function
    response

---
+++ pages/homepage/typescript/main.js
@@ -0,0 +1,11 @@
+//Trevor Colby
+//12.26.2017
+//Main Typescript for homepage of dartmouth.life
+
+function paintBackground(){
+	alert("Function Called");
+	document.body.style.backgroundImage = 'trevorColby.github.io/media/homepage/pictures/HopPhoto.jpg'
+	console.log("background loaded");
+}
+
+window.onload = alert("Not Working");
