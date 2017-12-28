3bd444d3a88524bbc40050e34182126d0499e96c
trevorColby <trevor.john.colby@gmail.com>
2017-12-28 00:24:27 -0500

    Added sticky effect to textBox nav, added func to main.js and .sticky to main.css

--- pages/homepage/typescript/main.js
+++ pages/homepage/typescript/main.js
@@ -2,8 +2,27 @@
 //12.26.2017
 //Main Typescript for homepage of dartmouth.life
 
-function paintBackground(){
-	console.log("background loaded");
+function scrollEffect(){
+
 }
 
-window.onload = paintBackground();
+// window.onload = scrollEffect();
+window.addEventListener('scroll',function(){
+	stickTextBox();
+}
+
+function calcScrollFactor(){
+	//var textOffset = window.offset.top(); alternet?
+	var offset = window.pageYoffset;
+}
+
+function stickTextBox(){
+	var textBox = document.getElementById("textBox");
+	var displacement = textBox.offSetTop;
+		if(window.pageYoffset >= displacement){
+			textBox.classList.add("sticky");
+		}
+		else {
+			textBox.classList.remove("sticky");
+		}
+}
