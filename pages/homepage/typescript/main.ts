88f600874b9887b685353daeecdb65b1e8277149
trevorColby <trevor.john.colby@gmail.com>
2018-07-24 22:29:22 -0400

    updated with alert comment out

--- pages/homepage/typescript/main.ts
+++ pages/homepage/typescript/main.ts
@@ -169,3 +169,5 @@ function paintBackground(){
 	document.body.style.backgroundImage = 'trevorColby.github.io/media/homepage/pictures/HopPhoto.jpg'
 	console.log("background loaded");
 }
+
+// window.onload = alert("Not Working");
