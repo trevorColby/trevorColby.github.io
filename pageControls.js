10b710261c0e63acb324b699380a7c6606ee08f2
trevorColby <trevor.john.colby@gmail.com>
2018-11-20 01:50:08 -0500

    Home button now closes menu as well on mobile

--- pageControls.js
+++ pageControls.js
@@ -1,10 +1,6 @@
 // Change style of navbar on scroll
 window.addEventListener( 'scroll', onWindowScroll, false );
-window.onload = hello;
 
-function hello() {
-	console.log("hello");
-}
 var transitionPoint = 10;
 // Modify navbar format for all pages
 function onWindowScroll(){
@@ -46,6 +42,29 @@ function toggleFunction() {
 	x.className += " w3-show";
 	navbar.className = " w3-bar" +  " w3-white";
     } else {
+	//if dropdown needs to be minimized
+	    // - make invisible
+	    // - remove shadow
+	x.className = x.className.replace(" w3-card", "");
+	x.className = x.className.replace(" w3-show", "");
+	//depending on page locaiton, modify uppermost navbar	
+	if (document.body.scrollTop > transitionPoint || document.documentElement.scrollTop > transitionPoint) {
+        	navbar.className = "w3-bar" + " w3-card" + " w3-white";
+	} else {
+        	navbar.className = " w3-bar";
+	}
+    }
+}
+
+
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function homeToggleFunction() {
+    //this one is the mobile dropdown
+    var x = document.getElementById("navDemo");
+    //this is the regular dropdown
+    var navbar = document.getElementById("myNavbar");
+    if (x.className.indexOf("w3-show") != -1) {
 	//if dropdown needs to be minimized
 	    // - make invisible
 	    // - remove shadow
