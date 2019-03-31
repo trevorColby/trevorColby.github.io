fb40f1bd2454bb69368be3d5b625a3abbde9960b
trevorColby <trevor.john.colby@gmail.com>
2019-03-31 18:14:29 -0400

    triAnimation.js: added better mobile detection and alternate logo animation for implode on mobile

--- projects/animeJS/triAnimation.js
+++ projects/animeJS/triAnimation.js
@@ -2,6 +2,7 @@
 function isMobileDevice() {
 	    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
 };
+
 var svgElement = document.getElementById('svgElem');
 if(isMobileDevice()) {
 	svgElement.style.height = "auto";
@@ -10,6 +11,16 @@ if(isMobileDevice()) {
 	svgElement.style.height = "50%";
 	svgElement.style.width = "50%";
 }
+
+
+//alternative way to test for mobile debatably more thorough
+var isMobile = false; //initiate as false
+// device detection
+if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) { 
+    isMobile = true;
+}
+
 
 
 //Formatting Solution for SVG animation of spotlight unerline
@@ -811,6 +822,7 @@ baCircle
 //************************************ Implode Transitions ************************************
 var stagImplode = anime.timeline({ autoplay: false, direction: 'alternate', loop: false });
 var wolfImplode = anime.timeline({ autoplay: false, direction: 'alternate', loop: false });
+var logoImplode = anime.timeline({ autoplay: false, direction: 'alternate', loop: false });
 
 //stag implode 
 sPaths.forEach(function(path, index) {
@@ -919,6 +931,62 @@ wolfImplode.add({
     easing: 'easeInCubic',
     offset: 3700
 });
+
+
+//logo implode
+cPaths.forEach(function(path, index) {
+logoImplode	 
+  .add({
+    targets: path.id,
+    d: {
+      value: path.d,
+      duration: 700,
+      easing: 'easeInCubic'
+    },
+    offset: 1000 + 10 * index
+    // offset:  5 * index
+  });
+});
+
+iPaths.forEach(function(path, index) {
+logoImplode	 
+  .add({
+    targets: path.id,
+    d: {
+      value: path.d,
+      duration: 200,
+      easing: 'easeInCubic'
+    },
+    // offset: 2600 
+    offset: 2600 + 3 * index
+  });
+});
+
+ePaths.forEach(function(path, index) {
+logoImplode	 
+  .add({
+    targets: path.id,
+    d: {
+      value: path.d,
+      duration: 150,
+      easing: 'easeInCubic'
+    },
+    offset: 3500 + 2 * index
+    // offset: 3500
+  });
+});
+
+// background 
+logoImplode.add({
+    targets: 'body',
+    // targets: '#pageSelect0C',
+    backgroundColor: '#e6e6e6',
+    duration: 200,
+    easing: 'easeInCubic',
+    offset: 3700
+});
+
+
 //************************************ Animal To Animal Transitions ************************************
 var stagWolf = anime.timeline({ autoplay: false, direction: 'alternate', loop: false });
 var stagBull = anime.timeline({ autoplay: false, direction: 'alternate', loop: false });
@@ -1420,23 +1488,22 @@ var hoverHandler = function(nextState,cssId){
 	animationQueue.push(state);	
 }
 
-window.onload = function(){
-	console.log('test');
-	setInterval(function(){
-		while(animationQueue.length > 0){
-			console.log('shift');
-			(animationQueue.shift())();	
-			setTimeout(function(){},700);
-		}
-	},10);
+// window.onload = function(){
+// 	setInterval(function(){
+// 		while(animationQueue.length > 0){
+// 			console.log('shift');
+// 			(animationQueue.shift())();	
+// 			setTimeout(function(){},700);
+// 		}
+// 	},10);
 
-	// setInterval(function(){
-	// 	console.log('second interval');
-	// 	if(timerCurr == 0){
-	// 		timerCurr = 1;
-	// 	}
-	// },1000);
-}//check which direction we stopped hovering in
+// 	// setInterval(function(){
+// 	// 	console.log('second interval');
+// 	// 	if(timerCurr == 0){
+// 	// 		timerCurr = 1;
+// 	// 	}
+// 	// },1000);
+// }//check which direction we stopped hovering in
 //	- if mouse left the bottom, trigger circle transition
 //	- otherwise exit should have no effect
 //: https://css-tricks.com/direction-aware-hover-effects/
@@ -1511,25 +1578,42 @@ var clickAnimation = function(click){
 		explode = false;
 	}
 	if(click == 1){
-		wolfImplode.seek(2500);
-		wolfImplode.play();
-		explode = true;
-		hideItem("pageSelect0C");	
-		hideItem("pageSelect1C");	
-		hideItem("pageSelect2C");	
-		hideItem("pageSelect3C");	
-		addItem('floatContainer','h1','pageTitle','Coding Projects',[]);
-		var tileContainer = document.getElementById('tileContainer');
-		var floatContainer = document.getElementById('floatContainer');
-		tileContainer.style.marginTop = floatContainer.getBoundingClientRect().height.toString() + 'px';
-		setTimeout(carouselLoad,1500);		
+		if(isMobile == false){
+			wolfImplode.seek(2500);
+			wolfImplode.play();
+			explode = true;
+			hideItem("pageSelect0C");	
+			hideItem("pageSelect1C");	
+			hideItem("pageSelect2C");	
+			hideItem("pageSelect3C");	
+			addItem('floatContainer','h1','pageTitle','Coding Projects',[]);
+			var tileContainer = document.getElementById('tileContainer');
+			var floatContainer = document.getElementById('floatContainer');
+			tileContainer.style.marginTop = floatContainer.getBoundingClientRect().height.toString() + 'px';
+			setTimeout(carouselLoad,1500);		
+			setTimeout(function(){hideItem("animationContainer");},1500);		
+		}else{
+			logoImplode.seek(2500);
+			logoImplode.play();
+			explode = true;
+			hideItem("pageSelect0C");	
+			hideItem("pageSelect1C");	
+			hideItem("pageSelect2C");	
+			hideItem("pageSelect3C");	
+			addItem('floatContainer','h1','pageTitle','Coding Projects',[]);
+			var tileContainer = document.getElementById('tileContainer');
+			var floatContainer = document.getElementById('floatContainer');
+			tileContainer.style.marginTop = floatContainer.getBoundingClientRect().height.toString() + 'px';
+			setTimeout(carouselLoad,1500);		
+			setTimeout(function(){hideItem("animationContainer");},1500);		
+		}	
 	}
 
 };
 
 var carouselLoad = function() {
 	addItem('tileContainer','div','carousel','',['carousel']);
-	document.getElementById('carousel').setAttribute('data-gap',80);
+	document.getElementById('carousel').setAttribute('data-gap',5);
 	addItem('carousel','figure','fig','',[]);
 	addItem('carousel','nav','nav','',[]);
 	addItem('nav','button','prev','Prev',['nav','prev']);
@@ -1599,7 +1683,7 @@ var carouselLaunch = function(){
 			figure.style.transformOrigin = `50% 50% ${- apothem}px`;
 
 			for (var i = 0; i < n; i++)
-				images[i].style.padding = `${gap}px`;
+				images[i].style.padding = `${gap}vw`;
 			for (i = 1; i < n; i++) {
 				images[i].style.transformOrigin = `50% 50% ${- apothem}px`;
 				images[i].style.transform = `rotateY(${i * theta}rad)`;
